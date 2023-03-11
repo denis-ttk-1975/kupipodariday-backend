14c4ae1b5cc623c4040d629ccf4356798c73897a
denis-ttk-1975 <denis.ttk@gmail.com>
2023-03-11 20:59:30 +0300

    entities connected to modules&app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { WishesModule } from './wishes/wishes.module';
 import { WishlistsModule } from './wishlists/wishlists.module';
 import { OffersModule } from './offers/offers.module';
+
+import { Wishlist } from './wishlists/entities/wishlist.entity';
+import { Wish } from './wishes/entities/wish.entity';
+import { User } from './users/entities/user.entity';
+import { Offer } from './offers/entities/offer.entity';
 
 @Module({
   imports: [
@@ -15,7 +21,7 @@ import { OffersModule } from './offers/offers.module';
       username: 'student',
       password: 'student',
       database: 'kupipodariday',
-      entities: [],
+      entities: [Wishlist, Wish, User, Offer],
       synchronize: true,
     }),
     UsersModule,
