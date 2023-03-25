f2d6d5c7fa1b642e5f4f1f494c829fe0a2239034
denis-ttk-1975 <denis.ttk@gmail.com>
2023-03-25 22:21:27 +0300

    wishes service imported to all others modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
+import { WishesModule } from './../wishes/wishes.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), WishesModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
